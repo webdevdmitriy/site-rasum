5fb5ee05dce29e1877df227d0223c66537890260
Dmitriy <plotnikovdi95@gmail.com>
2022-07-26 13:26:24 +0300

    Анимации теперь через gif

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,48 @@ const video = document.querySelector('.top__media-video')
 video.addEventListener('play', function () {
   console.log('play')
 })
+
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+anchors.forEach(link => {
+  link.addEventListener('click', function (e) {
+    e.preventDefault()
+    let href = this.getAttribute('href').slice(1)
+    const scrollTarget = document.getElementById(href)
+    const topOffset = document.querySelector('header').offsetHeight
+    const elementPosition = scrollTarget.getBoundingClientRect().top
+    const offsetPosition = elementPosition - topOffset
+    console.log(offsetPosition)
+    window.scrollBy({
+      top: offsetPosition,
+      behavior: 'smooth',
+    })
+  })
+})
+
+// onePageScroll('main', {
+//   sectionContainer: '.fp', // sectionContainer accepts any kind of selector in case you don't want to use section
+//   easing: 'ease', // Easing options accepts the CSS3 easing animation such "ease", "linear", "ease-in",
+//   // "ease-out", "ease-in-out", or even cubic bezier value such as "cubic-bezier(0.175, 0.885, 0.420, 1.310)"
+//   animationTime: 1000, // AnimationTime let you define how long each section takes to animate
+//   pagination: true, // You can either show or hide the pagination. Toggle true for show, false for hide.
+//   updateURL: false, // Toggle this true if you want the URL to be updated automatically when the user scroll to each page.
+//   beforeMove: function (index) {}, // This option accepts a callback function. The function will be called before the page moves.
+//   afterMove: function (index) {}, // This option accepts a callback function. The function will be called after the page moves.
+//   loop: false, // You can have the page loop back to the top/bottom when the user navigates at up/down on the first/last page.
+//   keyboard: true, // You can activate the keyboard controls
+//   responsiveFallback: true, // You can fallback to normal page scroll by defining the width of the browser in which
+//   // you want the responsive fallback to be triggered. For example, set this to 600 and whenever
+//   // the browser's width is less than 600, the fallback will kick in.
+// })
+
+// // moveDown('.top')
+
+const start = document.querySelector('.integration__animation .start')
+const main = document.querySelector('.integration__animation .main')
+
+setTimeout(function () {
+  // img.src = 'src/video/main.gif'
+  start.style.display = 'none'
+  main.style.display = 'block'
+}, 3900)
--- src/js/script.js
+++ src/js/script.js
@@ -10,7 +10,7 @@ function sleep(milliseconds) {
 }
 let ghfl = 0
 
-if (window.screen.width > 600) document.querySelector('.about__animation').innerHTML = "<img id='pat' src='https://m.protector-shin.ru/animations_tilda/image (4).png' type='img/base64'>"
+// if (window.screen.width > 600) document.querySelector('.about__animation').innerHTML = "<img id='pat' src='https://m.protector-shin.ru/animations_tilda/image (4).png' type='img/base64'>"
 
 var XHR = 'onload' in new XMLHttpRequest() ? XMLHttpRequest : XDomainRequest
 if (window.screen.width > 500) {
@@ -48,43 +48,44 @@ if (window.screen.width > 500) {
 
     xhhr.send()
 
-    // document.getElementsByClassName('shape')[0].innerHTML = "<img id='brain' src='https://инжиниринглаб.рф/brain.png' type='img/base64'>"
+    document.querySelector('.application__brain').innerHTML = "<img id='brain' src='https://инжиниринглаб.рф/brain.png' type='img/base64'>"
 
-    // var XHRx = 'onload' in new XMLHttpRequest() ? XMLHttpRequest : XDomainRequest
-    // var xhr = new XHRx()
-    // // (2) запрос на другой домен :)
-    // xhr.open('GET', 'https://инжиниринглаб.рф/brain.json', true)
-    // xhr.timeout = 100000
-    // xhr.onload = function () {
-    //   let s = this.responseText
-    //   fl = 1
-    //   let arra = []
-    //   let i = 0
-    //   let a = '[ '
-    //   while (i < 240) {
-    //     if (i > 0) {
-    //       a += ', '
-    //     }
-    //     a += '"'
-    //     let d = s.indexOf('"p":"') + 5
-    //     let tt = s.indexOf('"', d + 10)
-    //     let res = s.slice(d, tt)
-    //     s = s.slice(tt)
-    //     i = i + 1
-    //     a += res + '"'
-    //     arra[i - 1] = res
-    //   }
-    //   a += ']'
+    var XHRx = 'onload' in new XMLHttpRequest() ? XMLHttpRequest : XDomainRequest
+    var xhr = new XHRx()
+    // (2) запрос на другой домен :)
+    xhr.open('GET', 'https://инжиниринглаб.рф/brain.json', true)
+    xhr.timeout = 100000
+    xhr.onload = function () {
+      let s = this.responseText
+      fl = 1
+      let arra = []
+      let i = 0
+      let a = '[ '
+      while (i < 240) {
+        if (i > 0) {
+          a += ', '
+        }
+        a += '"'
+        let d = s.indexOf('"p":"') + 5
+        let tt = s.indexOf('"', d + 10)
+        let res = s.slice(d, tt)
+        s = s.slice(tt)
+        i = i + 1
+        a += res + '"'
+        arra[i - 1] = res
+      }
+      a += ']'
+      fred(0, arra, 'brain', 239, 50)
+    }
 
-    //   fred(0, arra, 'brain', 239, 50)
-    // }
+    xhr.onerror = function () {
+      alert('Ошибка ' + this.status)
+    }
 
-    // xhr.onerror = function () {
-    //   alert('Ошибка ' + this.status)
-    // }
-
-    // xhr.send()
+    xhr.send()
   }, 0)
+} else {
+  //frame('end');
 }
 
 let hl = 1
@@ -115,18 +116,18 @@ function fred(t, arra, sto, lo, p) {
 //===================================================================================
 //===================================================================================
 
-if (window.screen.width <= 500) {
-  document.querySelector('.tools__animation').innerHTML = "<video playsinline='true' muted='muted' controls='false' autoplay='false' id='last' type='video/mp4'>"
-  document.getElementById('last').style.width = '235px'
-  var vdig = document.getElementById('last')
-  vdig.src = 'https://инжиниринглаб.рф/an/dub.mp4'
-  vdig.controls = false
-  vdig.loop = false
-  vdig.autoplay = false
-} else {
-  document.querySelector('.tools__animation').innerHTML += "<img id='last' type='img/base64'>"
-  document.getElementById('last').src = 'https://m.protector-shin.ru/animations_tilda/imag.png'
-}
+// if (window.screen.width <= 500) {
+//   document.querySelector('.tools__animation').innerHTML = "<video playsinline='true' muted='muted' controls='false' autoplay='false' id='last' type='video/mp4'>"
+//   document.getElementById('last').style.width = '235px'
+//   var vdig = document.getElementById('last')
+//   vdig.src = 'https://инжиниринглаб.рф/an/dub.mp4'
+//   vdig.controls = false
+//   vdig.loop = false
+//   vdig.autoplay = false
+// } else {
+//   document.querySelector('.tools__animation').innerHTML += "<img id='last' type='img/base64'>"
+//   // document.getElementById('last').src = 'https://m.protector-shin.ru/animations_tilda/imag.png'
+// }
 function dolist(t, arra, sto, lo, p, k) {
   let img = document.getElementById(sto)
 
@@ -206,3 +207,179 @@ window.addEventListener('scroll', function () {
     }
   }
 })
+
+//==========================================================================================
+//==========================================================================================
+//==========================================================================================
+//==========================================================================================
+//==========================================================================================
+
+// let fig = 1
+// if (window.screen.width < 500) {
+//   document.getElementsByClassName('shape4')[0].innerHTML =
+//     "<video playsinline='true' muted='muted' controls='false' autoplay='false' id='stan' src='https://инжиниринглаб.рф/an/machine_start.mp4' type='video/mp4'></video>"
+
+//   document.getElementById('stan').style.width = '265px'
+//   document.getElementById('stan').controls = false
+//   document.getElementById('stan').loop = false
+//   document.getElementById('stan').autoplay = false
+// } else {
+//   document.querySelector('.integration__animation').innerHTML = "<img id='stan' type='img/base64'>"
+//   document.getElementById('stan').src = 'https://инжиниринглаб.рф/an/image.png'
+// }
+
+// function hoo() {
+//   let xgf = new XHR()
+//   xgf.open('GET', 'src/json/start.json', true)
+//   xgf.onload = function () {
+//     let s = this.responseText
+//     fl = 1
+//     let arra = []
+//     if (fig) {
+//       let i = 0
+//       let a = 'let f = [ '
+//       while (i < 184) {
+//         if (i > 0) {
+//           a += ', '
+//         }
+//         a += '"'
+//         let d = s.indexOf('"p":"') + 5
+//         let tt = s.indexOf('"', d + 10)
+//         let res = s.slice(d, tt)
+//         s = s.slice(tt)
+//         i = i + 1
+//         a += res + '"'
+//         arra[i - 1] = res
+//       }
+//       a += '];'
+
+//       dolistal(0, arra, 'stan', 183, 0)
+//     }
+//   }
+
+//   xgf.onerror = function () {
+//     alert('Ошибка ' + this.status)
+//   }
+
+//   xgf.send()
+// }
+
+// function fun() {
+//   let x = new XHR()
+//   x.open('GET', 'https://инжиниринглаб.рф/loop.json', true)
+//   x.onload = function () {
+//     let s = this.responseText
+//     hl = 1
+//     let arrsa = []
+//     let i = 0
+//     let a = 'let f = [ '
+//     while (i < 67) {
+//       if (i > 0) {
+//         a += ', '
+//       }
+//       a += '"'
+//       let d = s.indexOf('"p":"') + 5
+//       let tt = s.indexOf('"', d + 10)
+//       let res = s.slice(d, tt)
+//       s = s.slice(tt)
+//       i = i + 1
+//       a += res + '"'
+//       arrsa[i - 1] = res
+//     }
+//     a += '];'
+
+//     fred(0, arrsa, 'stan', 66, 0)
+//   }
+
+//   x.onerror = function () {
+//     alert('Ошибка ' + this.status)
+//   }
+
+//   x.send()
+// }
+
+// const imgs = document.querySelector('.imgs')
+// let lastAtr
+// let firstImg
+// let count = 1
+// flag = 0
+// function dolistal(t, arra, sto, lo, p) {
+//   let img = document.getElementById(sto)
+
+//   let hs = 1
+
+//   img.src = arra[t]
+
+//   if (count === 170) {
+//     return
+//   }
+
+//   if (lastAtr != arra[t]) {
+//     if (count == 2) {
+//       firstImg = arra[t]
+//     }
+//     // let imgsItem = document.createElement('img')
+
+//     // imgsItem.src = arra[t]
+//     // imgsItem.dataset.imgId = count
+//     // imgs.append(count)
+//     // imgs.append(imgsItem)
+//     // count++
+//     // lastAtr = arra[t]
+
+//     // if (firstImg == arra[t]) {
+//     //   flag = 1
+//     //   console.log('Начало')
+//     // }
+//   }
+
+//   //alert(t);
+//   if (t >= lo) {
+//     //
+//     if (lo == 33) {
+//       hs = 0
+//     } else {
+//       hs = 0
+//     }
+//     dolistal(154, arra, sto, lo, p)
+//   }
+//   t = t + 1
+//   if (hs) {
+//     img.onload = function () {
+//       sleep(p)
+//       setTimeout(dolistal(t, arra, sto, lo, p), 100)
+//     }
+//   }
+// }
+
+// window.addEventListener('scroll', function () {
+//   var pk = 3450
+//   if (window.screen.width > 1575) pk = 3250
+
+//   rl = 0
+//   if (window.screen.width < 500) {
+//     var vist = document.getElementById('stan')
+//     vist.style.width = '265px'
+//     $('#stan').css('z-index', '23')
+//     $('#stan').css('position', 'absolute')
+
+//     $('#stan').css('left', '0px')
+//     $('#stan').css('top', '0px')
+
+//     vist.controls = false
+//     vist.loop = false
+//     vist.autoplay = true
+//     //vist.load();
+//     vist.play()
+//     document.getElementById('stan').addEventListener('ended', myHandler, false)
+
+//     function myHandler(e) {
+//       var vis = document.getElementById('stan')
+
+//       vis.currentTime = 2.65
+//       vist.play()
+//     }
+//   } else {
+//     hoo()
+//   }
+// })
